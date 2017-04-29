b24345126ecbbdbd517d67505e7afdcb6f44c39e
r-stoyanov <rumen_stoyanoff@abv.bg>
2017-04-29 15:12:40 +0300

    coffee-machine version2

--- rumen/coffee-machine/machine.js
+++ rumen/coffee-machine/machine.js
@@ -1,3 +1,4 @@
+/*global document*/
 function CoffeeMachine() {
     this.coffee = 0;
     this.milk = 0;
@@ -6,36 +7,38 @@ function CoffeeMachine() {
 }
 var machine = new CoffeeMachine();
 
-CoffeeMachine.prototype.smallLoad = function () {
+CoffeeMachine.prototype.smallLoad = function() {
     this.coffee += 100;
     this.milk += 200;
     this.water += 300;
     this.cups += 10;
 };
-CoffeeMachine.prototype.mediumLoad = function () {
+CoffeeMachine.prototype.mediumLoad = function() {
     this.coffee += 500;
     this.milk += 300;
     this.water += 1000;
     this.cups += 30;
 };
-CoffeeMachine.prototype.bigLoad = function () {
+CoffeeMachine.prototype.bigLoad = function() {
     this.coffee += 1000;
     this.milk += 500;
     this.water += 2000;
     this.cups += 60;
 };
-CoffeeMachine.prototype.htmlStatus = function () {
-    return ("coffee: " + this.coffee + " gr, " + "milk: " + this.milk + " ml, " + "water: " + this.water + " ml, " + "cups: " + this.cups + " pcs");
+CoffeeMachine.prototype.htmlStatus = function() {
+    return ("<div class=\"well\">coffee: " + this.coffee + " gr</div>" + "<div class=\"well\">milk: " + this.milk + " ml</div>" + "<div class=\"well\">water: " + this.water + " ml</div>" + "<div class=\"well\">cups: " + this.cups + " pcs</div>");
 };
-CoffeeMachine.prototype.order = function (beverage) {
-    if ((this.coffee >= RECEPES[beverage].coffee) && (this.milk >= (RECEPES[beverage].milk || 0)) && (this.water >= RECEPES[beverage].water) && (this.cups >= RECEPES[beverage].cups)) {
+CoffeeMachine.prototype.order = function(beverage) {
+    if (RECEPES[beverage] === undefined) {
+        throw new Error("<div class=\"well\">Uknown beverage</div>");
+    } else if ((this.coffee >= RECEPES[beverage].coffee) && (this.milk >= (RECEPES[beverage].milk || 0)) && (this.water >= RECEPES[beverage].water) && (this.cups >= RECEPES[beverage].cups)) {
         this.coffee -= RECEPES[beverage].coffee;
         this.milk -= RECEPES[beverage].milk || 0;
         this.water -= RECEPES[beverage].water;
         this.cups -= RECEPES[beverage].cups;
         return this;
     } else {
-        throw new Error("Not enough components");
+        throw new Error("<div class=\"well\">Not enough components</div>");
     }
 
 };
@@ -77,4 +80,3 @@ var RECEPES = {
 
 document.getElementById("statusContainer").innerHTML = machine.htmlStatus();
 //console.log(recepes.coffee);
-//machine.order("coffee");
